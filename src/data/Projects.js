09c40f7a2b8f0424f933f0504267645a29cc75f6
dan1sh15 <danishqayyum9793@gmail.com>
2024-01-24 23:38:06 +0530

    new commit

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -23,7 +23,7 @@ const projects = [
     },
     {
         title: "News App",
-        description: "It is modern news Web Application built using React and Tailwind. The news app has a vertical navbar which helps in viewing the news according to the categories listed in it. The news is fetched from the NewsApi website. It has an infinte scroll page which is implemented by using the InfiniteScroll component. The project wasn’t hosted on any platform beacause the News Api was restricted for localhost request only.",
+        description: "It is modern news Web Application built using React and Tailwind. The news app has a vertical navbar which helps in viewing the news according to the categories listed in it. The news is fetched from the NewsApi website. It has an infinte scroll page which is implemented by using the InfiniteScroll component. The project wasn't hosted on any platform beacause the News Api was restricted for localhost request only.",
         techStack: "React and Tailwind",
         link: 'https://drive.google.com/file/d/1bEDYEcwZk8dzNaPEScedy6ucP3huytsG/view',
         githubLink: 'https://github.com/dan1sh15/News-App',
